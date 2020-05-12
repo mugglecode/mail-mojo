f46e8b0d31cb44dae4750eceaa6f190ab8ec8d88
Kelly <guojiyou40020@hotmail.com>
2020-05-12 15:02:14 +0800

    rename methods

--- smtp_magic/message.py
+++ smtp_magic/message.py
@@ -15,12 +15,12 @@ class Message:
     def add_text(self, text: str):
         self.msg.attach(MIMEText(text + '\n', 'plain', 'utf-8'))
 
-    def add_Image(self, path: str, name: str = None):
+    def add_image(self, path: str, name: str = None):
         with open(path, 'rb') as f:
             img = MIMEImage(f.read(), name=name if name is not None else os.path.basename(path))
         self.msg.attach(img)
 
-    def add_other_file(self, path: str, name: str = None):
+    def add_attachment(self, path: str, name: str = None):
         with open(path, 'rb') as f:
             attachment = MIMEApplication(f.read(), name=name if name is not None else os.path.basename(path))
         attachment['Content-Disposition'] = f'attachment; filename={name if name is not None else os.path.basename(path)}'

fb7fdd9ffc5982caf755eff3b59e70a35adc19bb
Kelly <guojiyou40020@hotmail.com>
2020-05-12 15:21:41 +0800

    update

--- smtp_magic/recieved_email.py
+++
@@ -1,6 +0,0 @@
-class Email:
-    def __init__(self, from_addr, to_addr, subj, content):
-        self.From = from_addr
-        self.To = to_addr
-        self.Subject = subj
-        self.Content = content

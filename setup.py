0739875323e373290850ed6b63dbf31e8d8a8ce8
Kelly <guojiyou40020@hotmail.com>
2020-05-12 11:08:35 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='smtp-magic',
+        version='0.1.0',
+        packages=['smtp_magic'],
+        url='',
+        license='GPL',
+        author='Kelly',
+        author_email='',
+        description=''
+)

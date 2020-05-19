894f6b8b962702042a4957483a8f34a4e40c709f
Kelly <guojiyou40020@hotmail.com>
2020-05-19 10:55:55 +0800

    update

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
         name='mail-mojo',
-        version='0.1.0',
+        version='0.1.1',
         packages=['mail_mojo'],
         url='',
         license='GPL',
         author='Kelly',
         author_email='',
         description='',
+        install_requires=['exchangelib'],
 )

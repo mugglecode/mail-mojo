e32e824162104be6241f7dbd40aab82c9ebe259e
Kelly <guojiyou40020@hotmail.com>
2020-06-12 14:31:53 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='mail-mojo',
-        version='0.1.1',
+        version='0.1.2',
         packages=['mail_mojo'],
         url='',
         license='GPL',
